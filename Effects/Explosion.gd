25c58455633dbffbf79ab323a4d3c8b2810875fc
nmccoy21 <nomccoy@iu.edu>
2022-02-05 16:41:21 -0500

    Completes the exercise

---
+++ Effects/Explosion.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite
+
+
+
+func _ready():
+	play("default")
+
+
+
+
+func _on_Explosion_animation_finished():
+	queue_free()
--- Enemy/Bullet.gd
+++ Enemy/Bullet.gd
@@ -29,3 +29,4 @@ func _on_Area2D_body_entered(body):
 
 func _on_Timer_timeout():
 	queue_free()
+
